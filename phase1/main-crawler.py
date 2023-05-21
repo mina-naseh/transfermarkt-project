faea15868f80a2e6828d4c5bc0af96a03f84cc65
Maryam Ahmadinejad <maryam.ahmadinejad91@gmail.com>
2023-05-21 23:06:33 +0330

    ✨add seasons and countries to main-crawler file

--- phase1/main-crawler.py
+++ phase1/main-crawler.py
@@ -11,6 +11,14 @@ headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
             'Accept-Language': 'en-US,en;q=0.5'
         }
+seasons = [2015, 2016, 2017, 2018, 2019, 2020, 2021]
+countries = [    
+    {"name":"Germany", "id":40},
+    {"name":"France", "id":50},
+    {"name":"Italy", "id":75},
+    {"name":"Spain", "id":157},
+    {"name":"England", "id":189},
+]
 
 
 #### CLUB(PLAYERS LIST) PAGE
