025c8bf6b072a8139ade1b29f7e33c7c32004e7f
Maryam Ahmadinejad <maryam.ahmadinejad91@gmail.com>
2023-05-23 07:24:37 +0330

    ✨read clubs_list from teams.json for crawl_players_list

--- phase1/crawlers/club_players_crawler.py
+++ phase1/crawlers/club_players_crawler.py
@@ -26,7 +26,13 @@ def get_players_data(content, players_initial_data, season, club_id, players_clu
         player_id = compact_player_href.rsplit('/')[-1]
         player_foot = player.select_one('td:nth-child(7)').text
         player_market_value = player.select_one('td:nth-child(11)').text
-        players_initial_data.append({"formatted_name": player_formatted_name, "id": player_id, "foot": player_foot})
+        player_info = {
+            "id": player_id,
+            "formatted_name": player_formatted_name,
+            "foot": player_foot
+        }
+        if player_info not in players_initial_data:
+            players_initial_data.append(player_info)
         players_club_history.append({
             "player_id": player_id,
             "season": season,
@@ -35,14 +41,20 @@ def get_players_data(content, players_initial_data, season, club_id, players_clu
         })
 
 
-def crawl_players_list(base_url, headers, clubs_list, seasons):
+def crawl_players_list(base_url, headers):
     players_initial_data = []
     players_club_history = []
-    for club in clubs_list:
-        for season in seasons:
-            club_players_url = f'{base_url}/{club["name"]}/kader/verein/{club["id"]}/plus/1/galerie/0?saison_id={season}'
+    with open('./crawlers/teams_data.json') as clubs_json_file:
+        clubs = json.load(clubs_json_file)
+        for club in clubs:
+            print(f"teat data is: {club}")
+            club_players_url = f'{base_url}/{club["name"]}/kader/verein/{club["id"]}/plus/1/galerie/0?saison_id={club["year"]}'
             response = requests.get(club_players_url, headers=headers)
             content = BeautifulSoup(response.text, 'html.parser')
-            get_players_data(content, players_initial_data, season, club["id"], players_club_history)
+            get_players_data(content, players_initial_data, club["year"], club["id"], players_club_history)
+    
+    with open("players_initial_data.json", "w") as json_players_initial_file:
+        json.dump(players_initial_data, json_players_initial_file, indent=4)
+    
     with open("players_club_history.json", "w") as json_players_club_history_file:
         json.dump(players_club_history, json_players_club_history_file, indent=4)
