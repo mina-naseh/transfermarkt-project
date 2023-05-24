34df6c05d11b3fc683db6b60de0e528d86c2d238
Maryam Ahmadinejad <maryam.ahmadinejad91@gmail.com>
2023-05-24 20:39:00 +0330

    ✨add team tables

--- phase2/db.py
+++ phase2/db.py
@@ -1,23 +1,23 @@
 from typing import List
 
-from sqlalchemy import URL, VARCHAR, Integer, create_engine, text, ForeignKey
+from sqlalchemy import (URL, VARCHAR, BigInteger, Float, ForeignKey, Integer,
+                        create_engine, text)
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-
 
 MYSQL_DRIVER = "mysql+mysqlconnector"
 MYSQL_USERNAME = "root"
-MYSQL_PASSWORD = "XXXXXXXXXXX"
+MYSQL_PASSWORD = "M13121371m$"
 MYSQL_HOST_NAME = "localhost"
 MYSQL_PORT = 3306
 DB_NAME = "transfermarktdb"
 
 
 url_object = URL.create(
-    MYSQL_DRIVER ,
-    username = MYSQL_USERNAME,
-    password = MYSQL_PASSWORD,
-    host = MYSQL_HOST_NAME,
-    port = MYSQL_PORT
+    MYSQL_DRIVER,
+    username=MYSQL_USERNAME,
+    password=MYSQL_PASSWORD,
+    host=MYSQL_HOST_NAME,
+    port=MYSQL_PORT,
 )
 engine = create_engine(url_object)
 
@@ -27,11 +27,11 @@ with engine.connect() as conn:
     conn.execute(text(f"CREATE DATABASE {DB_NAME}"))
 
 url_object = URL.create(
-    MYSQL_DRIVER ,
-    username = MYSQL_USERNAME,
-    password = MYSQL_PASSWORD,
-    host = MYSQL_HOST_NAME,
-    port = MYSQL_PORT,
+    MYSQL_DRIVER,
+    username=MYSQL_USERNAME,
+    password=MYSQL_PASSWORD,
+    host=MYSQL_HOST_NAME,
+    port=MYSQL_PORT,
     database=DB_NAME,
 )
 
@@ -56,6 +56,42 @@ class League(Base):
     id: Mapped[int] = mapped_column(Integer(), primary_key=True)
     name: Mapped[str] = mapped_column(VARCHAR(20))
     country_id: Mapped[int] = mapped_column(ForeignKey("country.id"))
+    team_details: Mapped[List["TeamDetail"]] = relationship(back_populates="league")
+
+
+class Team(Base):
+    __tablename__ = "team"
+
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    name: Mapped[str] = mapped_column(VARCHAR(20))
+    team_details: Mapped[List["TeamDetail"]] = relationship(back_populates="team")
+
+
+class TeamDetail(Base):
+    __tablename__ = "team_detail"
+
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    team_id: Mapped[int] = mapped_column(ForeignKey("team.id"))
+    year: Mapped[int] = mapped_column(Integer())
+    league_id: Mapped[int] = mapped_column(ForeignKey("league.id"))
+    average_age: Mapped[float] = mapped_column(Float())
+    match_played: Mapped[int] = mapped_column(Integer())
+    won: Mapped[int] = mapped_column(Integer())
+    draw: Mapped[int] = mapped_column(Integer())
+    lost: Mapped[int] = mapped_column(Integer())
+    goal_for: Mapped[int] = mapped_column(Integer())
+    goal_against: Mapped[int] = mapped_column(Integer())
+    goal_diff: Mapped[int] = mapped_column(Integer())
+    points: Mapped[int] = mapped_column(Integer())
+    group_position: Mapped[int] = mapped_column(Integer())
+    total_market_value: Mapped[int] = mapped_column(BigInteger())
+
+
+class PlayingPosition(Base):
+    __tablename__ = "playing_position"
+
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    name: Mapped[str] = mapped_column(VARCHAR(15))
 
 
 Base.metadata.create_all(bind=engine)
